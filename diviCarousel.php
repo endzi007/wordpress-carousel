df10a5b5f33538b91afac100dd77e3e3357a6c0d
Enis Jasarovic <jasarovic.enis@gmail.com>
2019-09-20 15:36:49 +0200

    Updating from live site

--- diviCarousel.php
+++ diviCarousel.php
@@ -28,24 +28,27 @@ function registerNgCarouselShortcode ($attr){
       'kategorija'=> "eventi",
       'grupe'=> 'false',
       'naslov'=> "",
+	  'visina'=> "200px"
     ), $attr);
 
-    $sirina = "66%";
+    $sirina = "90%";
     $shortcodeGroupValue = (int)$newAtts['grupe'];
     if($shortcodeGroupValue === 0){
-      $sirina = "66%";
+      $sirina = "90%";
     } elseif ($shortcodeGroupValue > 0 && $shortcodeGroupValue < 6 ) {
       $calculatedWidth = 100/($shortcodeGroupValue); 
       $sirina = $calculatedWidth . "%";
     } else{
-      $sirina = "66%";
+      $sirina = "90%";
     }
 
     $naslov =  $newAtts['naslov'];
     $grupe = $newAtts['grupe'];
-    $htmlContent = "<h3> $naslov </h3>";
-    $htmlContent .= "<div class='main-carousel' data-group=$shortcodeGroupValue style='width: 100%'>";
     $cat_id = get_cat_ID($newAtts['kategorija']);
+    $kategorijaURL = get_category_link( $cat_id );
+	$visina = $newAtts['visina'];
+    $htmlContent = "<h3><a href='$kategorijaURL'>$naslov</a></h3>";
+    $htmlContent .= "<div class='main-carousel' data-group=$shortcodeGroupValue style='width: 100%; min-height:$visina'>";
 
     //need to get events passed
     $posts = get_posts(array("category"=>$cat_id, "numberposts"=>10));
@@ -54,8 +57,8 @@ function registerNgCarouselShortcode ($attr){
         $thumbnailUrl= get_the_post_thumbnail_url($post->ID);
         $post_excerpt = $post->post_excerpt;
         $link = $post->guid;
-        print_r($link);
-        $htmlContent .= "<div class='carousel-cell' style= 'background: no-repeat center/cover url($thumbnailUrl); width: $sirina;'>";
+        $htmlContent .= "<div class='carousel-cell' style= 'width: $sirina;'>";
+		$htmlContent .= "<div style= 'background: no-repeat center/cover url($thumbnailUrl); width:100%; height:$visina; border-radius: 7px;'></div>";
         $htmlContent .="<p style='display:block; width: 100%; background-color: white; color: black;'><a style='color: white;' href='$link'>$post_excerpt</a></p>";
         
         $htmlContent .="</div>";
